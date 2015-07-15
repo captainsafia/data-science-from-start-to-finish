78a5b90fe062bc7b0ff3dd4c3520c3660259e507
Safia Abdalla <seabdalla@gmail.com>
2015-07-15 00:07:47 -0700

    Added content for column splicing using Bash

--- clean.sh
+++ clean.sh
@@ -13,7 +13,9 @@ du -h crimes.csv
 ### "slice-file"
 sed -n '/01\/01\/2010/,$p' > crimes_since_2010.csv && du -h crimes_since_2010.csv
 
-### "remove-unneeded-geolocation"
+### "remove-unneeded-geolocation-1"
 head -1 crimes_since_2010.cv | sed 's/[^,]//g' | wc -c
+### "remove-unneeded-geolocation-2"
 cut -d, -f1-21 crimes_since_2010.csv > crimes_final.csv
+### "remove-unneeded-geolocation-3"
 du -h crimes_final.csv
